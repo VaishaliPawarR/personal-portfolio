ada0b0001493f2ce947b4b1942879f43023a1f7d
Vaishali Pawar <vaishalirpawar16@gmail.com>
2023-08-20 14:32:18 +0530

    App.js
    added Navbar

---
+++ src/pages/Home.js
@@ -0,0 +1,47 @@
+import React from "react";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import EmailIcon from "@material-ui/icons/Email";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import "../styles/Home.css";
+
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="about">
+        <h2>Hi, my name is vaishali</h2>
+        <div className="prompt">
+          <p> 
+            A software developer for learning and creating.
+          </p>
+          <LinkedInIcon />
+          <EmailIcon />
+          <GitHubIcon />
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-End</h2>
+            <span>Javascript </span>
+          </li>
+
+          <li className="item">
+            <h2>Back-End</h2>
+            <span>ReactJs, Html, Css </span>
+          </li>
+
+          <li className="item">
+            <h2>Languages</h2>
+            <span> Bootstrap, Typescript</span>
+          </li>
+
+        </ol>
+      </div>
+
+    </div>
+  )
+}
+
+export default Home;
